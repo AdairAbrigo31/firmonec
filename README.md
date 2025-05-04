dd4f6fd6b18d0b0d8d88cf315280ddc80841515d
adair <adairaxe31@hotmail.com>
2025-05-04 01:29:21 -0500

    feat: readme more especific

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # firmonec
-Proyecto realizado en flutter que permite el acceso a cuentas Quipux y el firmado electronico de sus respectivos documentos
+Proyecto realizado en flutter que permite el acceso a cuentas Quipux y el firmado electronico de sus respectivos documentos.
 
 ## Versiones criticas
 Usar versiones iguales o superiores a flutter 3.29
+
+## Variables de entorno
+ROUTE_API : Es la direccion ip en donde este corriendo el backen "...IP.../api"
+TENANT_ID: Codigo generado al crear una aplicación en Microsoft entraID
+CLIENT_ID: Codigo generado al crear una aplicación en Microsoft entraID
+Para la simulacion de un servidor desplegado se uso ngrok
--- lib/configuration/microsoftID.dart
+++ lib/configuration/microsoftID.dart
@@ -1,11 +1,15 @@
 import 'package:aad_oauth/model/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'go_router.dart';
 
 class MicrosoftID {
+  static final String tenant = dotenv.env["TENANT_ID"] ?? "";
+  static final String clientId = dotenv.env["CLIENT_ID"] ?? "";
+
   static final Config config = Config(
-    tenant: "b7af8caf-83d8-4644-85ae-317c545223c1",
-    clientId: "c0c52924-3a1c-4d1d-9b63-7d08875207c8",
+    tenant: tenant,
+    clientId: clientId,
     scope: "openid profile email offline_access",
     redirectUri: "msauth://com.espol.firmonec2/callback",
     navigatorKey: navigatorKey,
@@ -14,6 +18,5 @@ class MicrosoftID {
         child: CircularProgressIndicator()), // Loader mientras se autentica
     // Para el postLogoutRedirectUri, dado que es una app móvil, podrías usar:
     postLogoutRedirectUri: "msauth://com.espol.firmonec2/callback",
-    // responseType: 'id_token token'
   );
 }
